2f5e06301c6254223c06f64669342acaed332f09
Allard <AllardQuek@users.noreply.github.com>
2020-06-23 00:11:30 +0800

    clean up

--- messenger.py
+++ messenger.py
@@ -29,21 +29,18 @@ from sys import argv
 from wit import Wit
 from bottle import Bottle, request, debug
 
+
 # Wit.ai parameters
 WIT_TOKEN = os.environ.get('WIT_TOKEN')
 # Messenger API parameters
 FB_PAGE_TOKEN = os.environ.get('FB_PAGE_TOKEN')
 # A user secret to verify webhook get request.
 FB_VERIFY_TOKEN = os.environ.get('FB_VERIFY_TOKEN')
-print(FB_PAGE_TOKEN)
 
 # Setup Bottle Server
 debug(True)
 app = Bottle()
 
-@app.get('/')
-def index():
-    return "NOTHING HERE"
 
 # Facebook Messenger GET Webhook
 @app.get('/webhook')
@@ -67,11 +64,14 @@ def messenger_post():
     """
     Handler for webhook (currently for postback and messages)
     """
+
     data = request.json
+
     if data['object'] == 'page':
         for entry in data['entry']:
             # get all the messages
             messages = entry['messaging']
+            # print(messages)
             if messages[0]:
                 # Get the first message
                 message = messages[0]
@@ -80,10 +80,9 @@ def messenger_post():
                 fb_id = message['sender']['id']
                 # We retrieve the message content
                 text = message['message']['text']
-                print(text, "TEXT")
                 response = client.message(text)
-                print(response)
-                # return "REPLY PLEASE"
+                print("RESPONSE:", response)
+                
                 # Let's forward the message to Wit /message
                 # and customize our response to the message in handle_message
                 # response = client.message(msg=text, context={'session_id':fb_id})
@@ -104,12 +103,44 @@ def fb_message(sender_id, text):
     }
     # Setup the query string with your PAGE TOKEN
     qs = 'access_token=' + FB_PAGE_TOKEN
+
     # Send POST request to messenger
     resp = requests.post('https://graph.facebook.com/me/messages?' + qs,
                          json=data)
     return resp.content
 
-
+def quick_reply(sender_id):
+    data = {
+          'recipient': {'id': sender_id},
+          'message': {
+              'text': "What would you like to do?",
+              'quick_replies': [
+                {
+                  "content_type":"text",
+                  "title":"Interval",
+                  "payload":"<POSTBACK_PAYLOAD>",
+                  # "image_url":"http://example.com/img/red.png"
+                },{
+                  "content_type":"text",
+                  "title":"Chord",
+                  "payload":"<POSTBACK_PAYLOAD>",
+                  # "image_url":"http://example.com/img/green.png"
+                },{
+                  "content_type":"text",
+                  "title":"Notes from Chord",
+                  "payload":"<POSTBACK_PAYLOAD",
+                }
+              ]
+          }
+    }
+    
+    qs = 'access_token=' + FB_PAGE_TOKEN
+    
+    # Send POST request to messenger
+    resp = requests.post('https://graph.facebook.com/me/messages?' + qs,
+                         json=data)
+    return resp.content
+    
 def first_trait_value(traits, trait):
     """
     Returns first trait value
@@ -121,6 +152,22 @@ def first_trait_value(traits, trait):
         return None
     return val
 
+def handle_start(fb_id):
+    """
+    Handle starting interaction
+    """
+    text = "Greetings! You can ask me about music theory or history, I would be happy to help!"
+    fb_message(fb_id, text)
+    quick_reply(fb_id)
+    
+def handle_gibberish(response, fb_id):
+    """
+    Handle whatever random gibberish user sends
+    """
+    text = f"Sorry! We did not quite understand \"{response['text']}\" :("
+    text_1 = "Try asking me about music theory or history!"
+    fb_message(fb_id, text)
+    fb_message(fb_id, text_1)
 
 def handle_message(response, fb_id):
     """
@@ -129,12 +176,35 @@ def handle_message(response, fb_id):
     # Checks if user's message is a greeting
     # Otherwise we will just repeat what they sent us
     greetings = first_trait_value(response['traits'], 'wit$greetings')
+    thanks = first_trait_value(response['traits'], 'wit$thanks')
+    bye = first_trait_value(response['traits'], 'wit$bye')
+
     if greetings:
-        text = "hello!"
+        handle_start(fb_id)
+    elif thanks:
+        text = "No problem!"
+    elif bye:
+        text = "Goodbye! Have a nice day :)"
     else:
-        text = "We've received your message: " + response['text']
-    # send message
-    fb_message(fb_id, text)
+        if not response['intents']:
+            handle_gibberish(response, fb_id)
+            return
+
+        intent = response['intents'][0]['name']
+        if intent == 'Greetings':
+            handle_start(fb_id)
+        elif intent == 'GetInterval':
+            # TODO: Identify the 2 notes user sent. Input to library function and return identified interval as response back to user.
+            text = "GETTING INTERVAL..."
+            fb_message(fb_id, text)
+        elif intent == 'getChords':
+            # TODO: Identify the notes user sent. Input to library function and return identified chord as response back to user.
+            # TODO: AND/OR Identify the chord user sent. Input to libary function and return chord's notes as response back to user.
+            text = "GETTING CHORDS..."
+            fb_message(fb_id, text)
+            pass
+        else:
+            text = "NO INTENT"
 
 
 # Setup Wit Client
