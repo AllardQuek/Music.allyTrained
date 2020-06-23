d542838408d3bd25d65e941b9d322f250e8001ae
versnow <63484110+versnow@users.noreply.github.com>
2020-06-24 01:12:56 +0800

    spotipy test file

---
+++ spotipydebugger.py
@@ -0,0 +1,19 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+cid = '06175aec93d14903bad4abb8ea0f16c7'
+secret = '45be25e4ab4a4f7888cd3b18e0d49983'
+client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+
+
+artist_name = []
+track_name = []
+popularity = []
+track_id = []
+for i in range(0,10000,50):
+    track_results = sp.search(q='year:2018', type='track', limit=50,offset=i)
+    for i, t in enumerate(track_results['tracks']['items']):
+        artist_name.append(t['artists'][0]['name'])
+        track_name.append(t['name'])
+        track_id.append(t['id'])
+        popularity.append(t['popularity'])
