82f80b8cf9d361a2b3e5da0516943dc2628c2b23
versnow <63484110+versnow@users.noreply.github.com>
2020-06-23 20:56:41 +0800

    Update getresponse.py

--- getresponse.py
+++ getresponse.py
@@ -1,8 +1,22 @@
+#contents = url.get("https://api.wit.ai/message?v=20200622&q=what is c major")
+
 # https://stackoverflow.com/questions/45868120/python-post-request-with-bearer-token?rq=1
 import requests
+import os, sys
+import subprocess
 
-text = "<PUT YOUR TEST TEXT HERE>"
+
+print ("Searching for:")
+text = input()
+
+#question="Enter grade for course" + str(i)
+#grade=input(question) *not sure how to get the inputs on the same line as string 'searching for:'
 
 response = requests.get(f"https://api.wit.ai/message?v=20200622&q={text}", headers={'Authorization': 'Bearer 4NQZLEUIKILQHV6PS7YPA3F5GXQ7GYLC'})
 response = response.json()
 print(response)
+
+print("Chatbot Restarted. Ask another question:")
+#os.execl("C:\Users\verly\Desktop\trialhttpget.py",  [""])
+#os.execl(sys.executable, sys.executable, * sys.argv)
+subprocess.call(sys.argv[0], shell=True)
