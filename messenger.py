44cbab1ea2f93f11e67ea1b4d1f15d48ad2ea01b
Allard <AllardQuek@users.noreply.github.com>
2020-06-23 19:52:08 +0800

    rearrange

--- messenger.py
+++ messenger.py
@@ -192,7 +192,6 @@ def handle_message(response, fb_id):
             return
 
         intent = response['intents'][0]['name']
-        entity = response['entities'][0]['name']
         
         if intent == 'Greetings':
             handle_start(fb_id)
@@ -261,16 +260,16 @@ def handle_message(response, fb_id):
                 item = f"{count}. {song['song']} ({song['section']}) by {song['artist']}\n"
                 text += item
                 count += 1
-        elif intent == 'getComposer' and entity == 'Baroque_Composer':
+        elif intent == 'getComposer' and response['entities'][0]['name'] == 'Baroque_Composer':
             text = "\"{response['text']}\" was a composer from the Baroque era, marked by little variations in tempo and 4/4 timings. Read more here: https://en.wikipedia.org/wiki/List_of_Baroque_composers"
-        elif intent == 'getComposer' and entity == 'Romantic_Composer':
+        elif intent == 'getComposer' and response['entities'][0]['name'] == 'Romantic_Composer':
             text = "\"{response['text']}\" was a composer from the Romantic era. Read more here: https://en.wikipedia.org/wiki/List_of_Romantic-era_composers"
         else:
             text = "Sorry, we couldn't quite understand. Please rephrase your question?"
 
     # Send response back to user
     fb_message(fb_id, text)
-    
+
         
 # Setup Wit Client
 client = Wit(access_token=WIT_TOKEN)
