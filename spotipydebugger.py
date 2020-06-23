321d6dc4bcc170a33ae9363a7e42f50b2949e4e6
versnow <63484110+versnow@users.noreply.github.com>
2020-06-24 01:48:50 +0800

    Update spotipydebugger.py

--- spotipydebugger.py
+++ spotipydebugger.py
@@ -1,19 +1,36 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-cid = '06175aec93d14903bad4abb8ea0f16c7'
-secret = '45be25e4ab4a4f7888cd3b18e0d49983'
+SPOTIPY_CLIENT_ID = '06175aec93d14903bad4abb8ea0f16c7'
+SPOTIPY_CLIENT_SECRET = '45be25e4ab4a4f7888cd3b18e0d49983'
+
 client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
-sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+"""
+#the below code works, to return a list of the names of all the albums released by the artist ‘Birdy’:
+birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
+spotify = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
 
-artist_name = []
-track_name = []
-popularity = []
-track_id = []
-for i in range(0,10000,50):
-    track_results = sp.search(q='year:2018', type='track', limit=50,offset=i)
-    for i, t in enumerate(track_results['tracks']['items']):
-        artist_name.append(t['artists'][0]['name'])
-        track_name.append(t['name'])
-        track_id.append(t['id'])
-        popularity.append(t['popularity'])
+results = spotify.artist_albums(birdy_uri, album_type='album')
+albums = results['items']
+while results['next']:
+    results = spotify.next(results)
+    albums.extend(results['items'])
+
+for album in albums:
+    print(album['name'])
+
+"""
+"""
+#the below code works, to print 30 second samples and cover art for the top 10 tracks for Led Zeppelin:
+lz_uri = 'spotify:artist:36QJpDe2go2KgaRleHCDTp'
+
+spotify = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+results = spotify.artist_top_tracks(lz_uri)
+
+for track in results['tracks'][:10]:
+    print('track    : ' + track['name'])
+    print('audio    : ' + track['preview_url'])
+    print('cover art: ' + track['album']['images'][0]['url'])
+    print()
+
+"""
